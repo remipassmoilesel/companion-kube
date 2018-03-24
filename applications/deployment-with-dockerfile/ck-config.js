16fdddd9a521c530f548cf56d3e10cfabec76baa
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-03-24 12:35:28 +0100

    Add require on app configs

--- applications/deployment-with-dockerfile/ck-config.js
+++ applications/deployment-with-dockerfile/ck-config.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
     projectType: 'deployment',
     docker: {
         build: false,
--- applications/helm-chart/ck-config.js
+++ applications/helm-chart/ck-config.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
     projectType: 'chart',
     releaseName: 'gitlab-ce'
 };
