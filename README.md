5f3ec13712f623c70bc3fe2c61ff525345c285ef
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-03-24 10:21:49 +0100

    Rename examples, improve cli tool

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# kube-server
+# companion-kube
 
 ## Introduction
 
 TODO :)
 
 ## Available commands
+
+     $ companion-kube list
+     $ companion-kube deploy
+     $ companion-kube stop
+     $ companion-kube restart
+     $ companion-kube rolling-update
+     $ companion-kube clear
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {mainConfig} from './config';
 
 import 'source-map-support/register';
 
+// TODO: remove
 info();
 info(`Initialized with configuration: ${JSON.stringify(mainConfig, null, 2)}`);
 info();
@@ -12,6 +13,14 @@ info();
 (async () => {
 
     try {
+
+        // companion-kube ?
+
+        // verifier la présence d'outils nécéssaires (helm, kubectl, ...)
+        // Proposer installation ?
+        // Proposer utilisation minikube ?
+        // Rechercher les applications disponibles
+        // Déployer ou stopper
 
     } catch (e) {
         error('Error while initializing Nginx configuration: ', e);
