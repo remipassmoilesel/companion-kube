c95c367d785c2f406064855e189cd5ad9abd0542
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-03-29 12:42:16 +0200

    Add first ingress, refactor example app

--- applications/deployment-with-dockerfile/ck-config.js
+++ applications/deployment-with-dockerfile/ck-config.js
@@ -5,7 +5,7 @@ module.exports = {
         imageName: 'deployment-with-dockerfile',
         tag: '0.1',
         push: false,
-        buildDirectory: '.'
+        buildDirectory: './docker-image'
     },
     scripts: {
         buildDev: 'docker build . -t deployment-with-dockerfile:0.1',
