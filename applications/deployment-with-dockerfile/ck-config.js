2d8de3f13e13e9b560da53f0ac76316b104673c5
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-03-30 12:24:42 +0200

    Fix script

--- applications/deployment-with-dockerfile/ck-config.js
+++ applications/deployment-with-dockerfile/ck-config.js
@@ -8,7 +8,7 @@ module.exports = {
         buildDirectory: './docker-image'
     },
     scripts: {
-        buildDev: 'docker build . -t deployment-with-dockerfile:0.1',
+        buildDev: 'docker build ./docker-image -t deployment-with-dockerfile:0.1',
         dev: 'ck run buildDev && docker run -p 8080:80 deployment-with-dockerfile',
         buildMinikube: 'eval $(minikube docker-env) && ck run buildDev'
     }
