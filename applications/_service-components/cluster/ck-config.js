18728d548268c5bd4048969de691304a3dcc1781
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-04-02 22:00:58 +0200

    Improve destroy task

--- applications/_service-components/cluster/ck-config.js
+++ applications/_service-components/cluster/ck-config.js
@@ -7,7 +7,7 @@ module.exports = {
                 path: '#/scripts/kubespray/cluster.yml -vv',
             },
             destroy: {
-                path: '#/scripts/kubespray/reset.yml -vv',
+                path: '#/scripts/kubespray/reset.yml  --extra-vars "reset_confirmation=yes" -vv',
             },
             scale: {
                 path: '#/scripts/kubespray/scale.yml -vv',
