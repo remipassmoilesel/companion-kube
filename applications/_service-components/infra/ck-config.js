1901e409b3f074031360229159b5fc2de8d2635a
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-04-02 16:18:25 +0200

    Restore traefik simple deployment

--- applications/_service-components/infra/ck-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    applicationStructure: 'chart',
-    helm: {
-        releaseName: 'infra'
-    },
-    scripts: {
-        debug: 'helm install --dry-run --debug .'
-    }
-};
