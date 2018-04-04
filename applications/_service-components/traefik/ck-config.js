be35a644ff0aa26744ad1a7e208f63194e9aa042
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-04-05 00:05:37 +0200

    Working on traefik

--- applications/_service-components/traefik/ck-config.js
+++ applications/_service-components/traefik/ck-config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    applicationStructure: 'deployment'
+    applicationStructure: 'deployment',
+    scripts: {
+        pushImages: 'ansible-playbook -i ../_cluster/inventory-dev.cfg playbook/push-images.yaml',
+        buildDev: 'docker build ./docker-image -t traefik-dev',
+        dev: 'ck run buildDev && docker run traefik-dev'
+    }
 };
