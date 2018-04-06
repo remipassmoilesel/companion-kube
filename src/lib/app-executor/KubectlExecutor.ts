631570b515b8349feec93cccfc699179dafa65b2
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-04-06 13:33:57 +0200

    Improve kubectl executor'

--- src/lib/app-executor/KubectlExecutor.ts
+++ src/lib/app-executor/KubectlExecutor.ts
@@ -1,32 +1,45 @@
-import {IMainConfig} from '../main-config/configTypes';
+import * as path from 'path';
+import _ = require('lodash');
 import {AbstractAppExecutor} from './AbstractAppExecutor';
 import {IKubeApplication} from '../app-config/appConfigTypes';
-import {execSync} from 'child_process';
 import {Logger} from '../misc/Logger';
 
 export class KubectlExecutor extends AbstractAppExecutor {
     public logger: Logger = new Logger();
-
-    constructor(mainConfig: IMainConfig) {
-        super(mainConfig);
-    }
 
     public isSupported(app: IKubeApplication): boolean {
         return app.applicationStructure === 'deployment';
     }
 
     public async deploy(app: IKubeApplication, envName?: string): Promise<any> {
-        const namespaceOption = envName ? `--namespace ${envName}` : '';
-        const command = `kubectl create ${namespaceOption} -f ${app.rootPath}`;
+        const paths = this.getRootPaths(app);
 
-        await this.execCommand(command, app.displayOutput);
+        for (const absolutePath of paths){
+            const namespaceOption = envName ? `--namespace ${envName}` : '';
+            const command = `kubectl create ${namespaceOption} -f ${absolutePath}`;
+
+            await this.execCommand(command, app.displayOutput);
+        }
     }
 
     public async destroy(app: IKubeApplication, envName?: string): Promise<any> {
-        const namespaceOption = envName ? `--namespace ${envName}` : '';
-        const command = `kubectl delete ${namespaceOption} -f ${app.rootPath}`;
+        const paths = this.getRootPaths(app);
 
-        await this.execCommand(command, app.displayOutput);
+        for (const absolutePath of paths){
+            const namespaceOption = envName ? `--namespace ${envName}` : '';
+            const command = `kubectl delete ${namespaceOption} -f ${absolutePath}`;
+
+            await this.execCommand(command, app.displayOutput);
+        }
     }
 
+    private getRootPaths(app: IKubeApplication): string[] {
+        if (!app.deployment || !app.deployment.roots){
+            return [app.rootPath];
+        }
+
+        return _.map(app.deployment.roots, (relativePath: string) => {
+            return path.join(app.rootPath, relativePath);
+        });
+    }
 }
