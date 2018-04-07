053058e6e4097d3e52d77ecc12b2dba1374f740e
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-04-07 17:04:00 +0200

    Finalize traefik deploymenté

--- applications/_service-components/traefik/ck-config.js
+++ applications/_service-components/traefik/ck-config.js
@@ -1,8 +1,15 @@
 module.exports = {
     applicationStructure: 'deployment',
+    deployment: {
+        roots: ['./deployments'],
+    },
     scripts: {
-        pushImages: 'ansible-playbook -i ../_cluster/inventory-dev.cfg playbook/push-images.yaml',
+        pushImage: 'ansible-playbook -i ../_cluster/inventory-dev.cfg playbook/push-image.yaml',
         buildDev: 'docker build ./docker-image -t traefik-dev',
-        dev: 'ck run buildDev && docker run traefik-dev'
+        dev: 'ck run buildDev && docker run traefik-dev',
+        version: 'ck run buildDev && docker run traefik-dev version'
+    },
+    hooks: {
+        preDeploy: 'ck run pushImage',
     }
 };
