d1eea328660cff9e773660fd2d610824fa15bb68
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2018-07-07 17:15:40 +0200

    Improve kube config script

--- examples/scripts/update-kube-config.sh
+++ examples/scripts/update-kube-config.sh
@@ -2,17 +2,15 @@
 
 # mardi 3 avril 2018, 21:57:33 (UTC+0200)
 
-MASTER_ADRESS=BobosseLeBoss
+MASTER_ADRESS=192.168.0.15
 REMOTE_CONFIG_PATH=/etc/kubernetes/admin.conf
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
-scp root@$MASTER_ADRESS:$REMOTE_CONFIG_PATH .
+scp root@$MASTER_ADRESS:$REMOTE_CONFIG_PATH config
 
-cd ~/.kube
+cp ~/.kube/config ~/.kube/config-$(date +%Y%m%d_%H%M%S).bak
 
-cp config config-$(date +%Y%m%d_%H%M%S).bak
-
-cp $SCRIPT_DIR/admin.conf config
+cp config ~/.kube/config
 
